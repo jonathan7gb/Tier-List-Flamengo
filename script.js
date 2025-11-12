01495005c9aa43329463dda650444986d4d79352
jonathan_uber <jonathanuber121207@gmail.com>
2025-11-11 21:43:33 -0300

    fix: arruma dropzone

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const draggables = document.querySelectorAll('.draggable')
 const dropzones = document.querySelectorAll('.dropzone')
 
+draggables.forEach((img, index) => {
+  // garante que cada imagem tem um id único
+  img.id = `player-${index}`;
 
-draggables.forEach(img => {
   img.addEventListener('dragstart', e => {
-    e.dataTransfer.setData('text/plain', e.target.src);
+    e.dataTransfer.setData('id', e.target.id); // guarda o id
     e.target.classList.add('opacity-50');
   });
 
@@ -14,32 +16,29 @@ draggables.forEach(img => {
 });
 
 dropzones.forEach(dropzone => {
-    // Evento dragenter - elemento entra na área de drop
-    dropzone.addEventListener('dragenter', (event) => {
-        event.preventDefault();
-        dropzone.classList.add('over');
-        console.log('Entrou na área de drop');
-    });
-    
-    // Evento dragover - elemento está sobre a área de drop
-    dropzone.addEventListener('dragover', (event) => {
-        event.preventDefault(); // Necessário para permitir o drop
-        console.log('Elemento sobre a área de drop');
-    });
-    
-    // Evento dragleave - elemento deixa a área de drop
-    dropzone.addEventListener('dragleave', () => {
-        dropzone.classList.remove('over');
-        console.log('Saiu da área de drop');
-    });
-    
-    // Evento drop - elemento é solto na área de drop
-     dropzone.addEventListener('drop', event => {
-        event.preventDefault();
-        dropzone.classList.remove('over');
+  dropzone.addEventListener('dragenter', event => {
+    event.preventDefault();
+    dropzone.classList.add('over');
+  });
 
-        const draggedId = event.dataTransfer.getData('id');
-        const draggedElement = document.getElementById(draggedId);
+  dropzone.addEventListener('dragover', event => {
+    event.preventDefault();
+  });
+
+  dropzone.addEventListener('dragleave', () => {
+    dropzone.classList.remove('over');
+  });
+
+  dropzone.addEventListener('drop', event => {
+    event.preventDefault();
+    dropzone.classList.remove('over');
+
+    const draggedId = event.dataTransfer.getData('id');
+    const draggedElement = document.getElementById(draggedId);
+
+    if (draggedElement) {
         dropzone.appendChild(draggedElement);
-    });
-})
+        draggedElement.classList.add('inDropzone')
+    }
+  });
+});
